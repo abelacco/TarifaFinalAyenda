ac37b405094f240184fc8e4c0f894da13151906a
abelacco <abel3121@gmail.com>
2022-04-29 11:05:09 -0500

    Ahora muestra cuanto es el igv y el tipo de cambio que se uso

--- tarifa.js
+++ tarifa.js
@@ -6,5 +6,7 @@ function convertirTarifa(){
     const tarifaAyenda = input.value;
     const tarifaFinal = Math.round(tarifaAyenda * igv * tipoCambio);
     const resultP = document.getElementById("ResultP");
+    const resultP2= document.getElementById("ResultP2");
     resultP.innerText = "El precio en soles y con igv es de: " + tarifaFinal;
+    resultP2.innerText = "El igv es: " + igvHab + "," + "Tipo de cambio: " + tipoCambio; 
 };
