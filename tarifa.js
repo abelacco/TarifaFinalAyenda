bd246f2ad8c473796f259bef1fba007bb51370af
abelacco <abel3121@gmail.com>
2022-05-12 19:14:30 -0500

    Se agrego habitacion2 y además se cambio el background y tiene firma de autor

--- tarifa.js
+++ tarifa.js
@@ -2,11 +2,28 @@ const igv = 1.18;
 const tipoCambio = 3.80;
 
 function convertirTarifa(){
-    const input = document.getElementById("hab1")
-    const tarifaAyenda = input.value;
+    const input = document.getElementById("hab1");
+    const noches = document.getElementById("noche");
+    const nochesValue = noches.value;
+    const value = input.value;
+    const tarifaAyenda = value;
     const tarifaFinal = Math.round(tarifaAyenda * igv * tipoCambio);
+    const porNoche = tarifaFinal / noches.value ;
     const resultP = document.getElementById("ResultP");
-    const resultP2= document.getElementById("ResultP2");
-    resultP.innerText = "El precio en soles y con igv es de: " + tarifaFinal;
-    resultP2.innerText = "El igv es: " + igvHab + "," + "Tipo de cambio: " + tipoCambio; 
+    resultP.innerText = "El precio en soles y con igv es de: " + tarifaFinal + "\nEl precio por noche es: " + porNoche;
+
 };
+
+function convertirTarifa2(){
+    const input2 = document.getElementById("hab2");
+    const noches = document.getElementById("noche2");
+    const nochesValue = noches.value;
+    const value2 = input2.value;
+    const tarifaAyenda = value2;
+    const tarifaFinal = Math.round(tarifaAyenda * igv * tipoCambio);
+    const porNoche = tarifaFinal / noches.value ;
+    const resultP2 = document.getElementById("ResultP2");
+    resultP2.innerText = "El precio en soles y con igv es de: " + tarifaFinal + "\nEl precio por noche es: " + porNoche;
+    
+};
+
